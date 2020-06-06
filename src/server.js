7d38f8155c0202d62e181b2c19f70879396370be
Gabriela Fernandes <57195976+grchf@users.noreply.github.com>
2020-06-06 18:18:18 -0300

    template-engine

--- src/server.js
+++ src/server.js
@@ -4,6 +4,13 @@ const server = express()
 // configurar pasta public (para ler na raiz do projeto)
 server.use(express.static("public"))
 
+// utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
 // configurar caminhos da minha aplicação
 // página inicial
 // req: requisição
@@ -13,11 +20,15 @@ ao fazer modificações no arquivo de servidor, é necessário reinicializar o s
 package para reiniciar automatico: nodemon
 */
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html", { title: "Meu título" })
 })
 
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
 })
 
 // ligar o servidor
