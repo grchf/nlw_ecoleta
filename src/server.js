19229cff9957e554031fe65ca6b5eba264aae589
Gabriela Fernandes <57195976+grchf@users.noreply.github.com>
2020-06-07 01:20:38 -0300

    final-aula-5

--- src/server.js
+++ src/server.js
@@ -7,6 +7,9 @@ const db = require("./database/db.js")
 // configurar pasta public (para ler na raiz do projeto)
 server.use(express.static("public"))
 
+// habilitar o uso do req.body na aplicação
+server.use(express.urlencoded( {extended: true} ))
+
 // utilizando template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("src/views", {
@@ -29,19 +32,65 @@ server.get("/", (req, res) => {
 server.get("/create-point", (req, res) => {
 
     // req.query = query strings da nossa url
-    console.log(req.query)
+    // console.log(req.query)
+
 
     return res.render("create-point.html")
 })
 
 server.post("/savepoint", (req, res) => {
-    return res.send("ok")
+
+    // req.body = o corpo do nosso formulário
+    // console.log(req.body)
+
+    // inserir dados no banco de dados
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+        `
+        const values = [
+            req.body.image,
+            req.body.name,
+            req.body.address,
+            req.body.address2,
+            req.body.state,
+            req.body.city,
+            req.body.items,
+        ]
+
+        function afterInsertData(err) {
+            if(err) {
+                console.log(err)
+                return res.send("Erro no cadastro!")
+            }
+
+            console.log("Cadastrado com sucesso!")
+            console.log(this)
+
+            return res.render("create-point.html", { saved: true })
+        }
+
+        db.run(query, values, afterInsertData)
+        
 })
 
 server.get("/search", (req, res) => {
 
+    const search = req.query.search
+    if(search == "") {
+        // pesquisa vazia
+        return res.render("search-results.html", { total: 0 })
+    }
+
     // pegar os dados do banco de dados
-    db.all(`SELECT * FROM places`, function(err, rows) {
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
         if(err) {
             return console.log(err)
         }
